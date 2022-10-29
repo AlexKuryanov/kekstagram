ebccf0fcc00b07f597035bb62ea434ad59906df8
AlexKuryanov <lexxus07@yandex.ru>
2022-10-29 22:06:43 +0200

    feat: create function to generate objects

--- js/main.js
+++ js/main.js
@@ -79,12 +79,40 @@ const getRandomNum = (min, max) => {
   return Math.floor(Math.random() * (max + 1 - min) + min);
 };
 
-console.log(getRandomNum(5, -1));
-
 const checkLength = (string, maxLength) => {
   return string.length <= maxLength;
 };
 
-let str =
-  'Lorem ipsum dolor';
-console.log(checkLength(str, 15));
+const getRandomArrayElement = (array) => {
+  return array[getRandomNum(0, array.length - 1)];
+}
+
+const createComment = () => {
+  const comments = [];
+  for (let i = 0; i < getRandomNum(CommentsCount.min, CommentsCount.max); i++) {
+    comments.push({
+      id: getRandomNum(RandomInterval.min, RandomInterval.max),
+      avatar: `img/avatar-${getRandomNum(AvatarCount.min, AvatarCount.max)}.svg`,
+      message: getRandomArrayElement(MESSAGES),
+      name: getRandomArrayElement(NAMES),
+    })
+  }
+  return comments;
+}
+
+const photos = [];
+const createDescription = () => {
+  for (let i = 1; i <= PHOTOS_COUNT; i++) {
+    photos.push({
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: DESCRIPTIONS[i - 1],
+      likes: getRandomNum(LikesCount.min, LikesCount.max),
+      comments: createComment(),
+    });
+  }
+  return photos;
+}
+
+createDescription();
+console.log(photos);
